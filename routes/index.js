593aa11d52f2268ac6c32c7868a87ab82e4765c2
Martin Nilsson <ibbles@hotmail.com>
2016-05-24 20:31:41 +0200

    Make routes/index.js conform to JSLint.

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,35 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
 var mongodb = require("mongodb");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
     res.render('index', {
         title: 'Recept',
-        name: "Bucky" });
+        name: "Bucky"
+    });
 });
 
-router.get("/thelist", function(reqest, response) {
-    var database = mongodb.MongoClient;
-    var url = "mongodb://localhost:27017/recipies";
-    database.connect(url, function(error, db) {
+router.get("/thelist", function (reqest, response) {
+    var database = mongodb.MongoClient,
+        url = "mongodb://localhost:27017/recipies";
+    database.connect(url, function (error, db) {
         if (error) {
             console.log("Unable to connect to server.", error);
         } else {
             console.log("Connection established.");
             var collection = db.collection("recipies");
-            collection.find({}).toArray(function(error, result) {
+            collection.find({}).toArray(function (error, result) {
                 if (error) {
                     response.send(error);
                 } else if (result.length) {
-                    console.log("About to render page with the following recipies:")
-                    result.forEach(function(recipie) {
-                        console.log(recipie)
+                    console.log("About to render page with the following recipies:");
+                    result.forEach(function (recipie) {
+                        console.log(recipie);
                     });
                     response.render('recipieslist', {
                         "recipieslist": result
