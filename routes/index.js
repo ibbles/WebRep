637518ab7901c0a87838bce3198e4ae6f80b1385
Martin Nilsson <ibbles@hotmail.com>
2016-05-24 20:10:31 +0200

    First attempt at reading recipes from the MongoDB database.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var mongodb = require("mongodb");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    res.render('index', {
+        title: 'Recept',
+        name: "Bucky" });
+});
+
+router.get("/thelist", function(reqest, response) {
+    var database = mongodb.MongoClient;
+    var url = "mongodb://localhost:27017/recipies";
+    database.connect(url, function(error, db) {
+        if (error) {
+            console.log("Unable to connect to server.", error);
+        } else {
+            console.log("Connection established.");
+            var collection = db.collection("recipies");
+            collection.find({}).toArray(function(error, result) {
+                if (error) {
+                    response.send(error);
+                } else if (result.length) {
+                    console.log("About to render page with the following recipies:")
+                    result.forEach(function(recipie) {
+                        console.log(recipie)
+                    });
+                    response.render('recipieslist', {
+                        "recipieslist": result
+                    });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
