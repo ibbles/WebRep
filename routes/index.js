776259e9cdc48eb6ce4f804b491427ba211a848d
Martin Nilsson <ibbles@hotmail.com>
2016-11-19 18:41:53 +0100

    Test recipelist implementation using promises.

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,47 @@ var express = require('express');
 var router = express.Router();
 
 var mongodb = require("mongodb");
-var Q = require("q");
+//var Q = require("q");
 
 
 
 router.get('/', function (req, res) {
     res.render('index', {
         title: 'Recept',
-        recipieslist: [{tip: "tip1"}, {tip: "tip2"}, {tip: "tip3"}]
+        recipeslist: [{tip: "tip1"}, {tip: "tip2"}, {tip: "tip3"}]
     });
+});
+
+
+router.get('/thelist_q', function(request, response) {
+    var database = mongodb.MongoClient;
+    var url = "mongodb://localhost:27017/recipes";
+    database.connect(url)
+        .then(function(db) {
+            console.log("Connection established.");
+            var collection = db.collection("recipes");
+            collection.find({}).toArray()
+                .then(function(result) {
+                    console.log("Got " + result.length + " recipes.");
+                    console.log("About to render page with the following recipes:");
+                    result.forEach(function (recipe) {
+                        console.log(recipe);
+                    });
+                    response.render('recipeslist', {
+                        "recipeslist": result
+                    });
+                });
+                /*
+
+                .error(function(error) {
+                    console.log("Unable to fetch recipes from database: "+error);
+                    response.end("Unable to fetch recipes from database");
+                })*/
+        })
+        .catch(function (error) {
+            console.log("Unable to connect to server.", error);
+            response.end("Web server could not connect to database.");
+        });
 });
 
 
@@ -27,18 +59,18 @@ router.get("/thelist", function (reqest, response) {
             response.end();
         } else {
             console.log("Connection established.");
-            var collection = db.collection("recipies");
+            var collection = db.collection("recipes");
             collection.find({}).toArray(function (error, result) {
                 if (error) {
                     response.send(error);
                 } else if (result.length) {
                     console.log("Got " + result.length + " recipes.");
-                    console.log("About to render page with the following recipies:");
+                    console.log("About to render page with the following recipes:");
                     result.forEach(function (recipie) {
                         console.log(recipie);
                     });
-                    response.render('recipieslist', {
-                        "recipieslist": result
+                    response.render('recipeslist', {
+                        "recipeslist": result
                     });
                 } else {
                     console.log("Got no recipes. Databse is empty.");
@@ -51,4 +83,3 @@ router.get("/thelist", function (reqest, response) {
 });
 
 module.exports = router;
-
