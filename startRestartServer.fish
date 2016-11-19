43e9e87a9b28302c70502ce5e1c697f1112514ee
Martin Nilsson <ibbles@hotmail.com>
2016-11-19 18:21:41 +0100

    Reimplement startRestartServer.fish
    For some reason killing node the way I used to do it now also kills the whole script. No idea why. Using another way of finding node's pid, which doesn't kill the script.

--- startRestartServer.fish
+++ startRestartServer.fish
@@ -1,23 +1,46 @@
 #!/usr/bin/fish
 
-set serverPid 0
+
 while test 1 -eq 1
     echo -e "\n---- BEGIN ----"
-    if test $serverPid -ne 0
-        set serverCommandLine (strings -1 /proc/$serverPid/cmdline)
-        if test "$serverCommandLine" = "node ./bin/www"
-            echo "A server already running. Stopping it."
-            kill -SIGINT $serverPid
-        else
-            echo "Command line is '$serverCommandLine' does not belong to a server we started. Not stopping."
+    node ./bin/www &
+    inotifywait -qe close_write **.js
+    echo -e "---- END ----\n"
+    for pid in (pgrep node)
+        set cmdLine (strings -1 /proc/$pid/cmdline)
+        if test "$cmdLine" = "node ./bin/www"
+            kill -SIGKILL $pid
         end
     end
-    echo "Starting server."
-    node ./bin/www &
-    set serverPid (jobs -lp)
-    echo "Server started with pid $serverPid."
-    sleep 1
-    echo "Waiting for restart signal."
-    inotifywait -qe close_write **.js
-    echo "Got restart signal."
 end
+
+
+## Old
+#
+#set serverPid 0
+#while test 1 -eq 1
+#    echo -e "\n---- BEGIN ----"
+#    if test $serverPid -ne 0
+#        set serverCommandLine (strings -1 /proc/$serverPid/cmdline)
+#        if test "$serverCommandLine" = "node ./bin/www"
+#            echo "A server already running with pid $serverPid. Stopping it."
+#            echo killing "'"(strings -1 /proc/$serverPid/cmdline)"'"
+#            kill -SIGINT $serverPid
+#            sleep 1
+#        else
+#            echo "Command line is '$serverCommandLine' for pid $serverPid does not belong to a server we started. Not stopping."
+#        end
+#    end
+#    echo "Starting server."
+#    node ./bin/www &
+#    set serverPid (jobs -lp | tail -n 1)
+#    echo "Server started with pid $serverPid."
+#    sleep 1
+#    echo "Waiting for restart signal."
+#    inotifywait -qe close_write **.js
+#    echo "Got restart signal."
+#end
+#
+#echo "Infinite while-loop terminated." 
+#
+#
