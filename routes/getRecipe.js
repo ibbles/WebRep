b88d16f90607f0e06bba1119f8e7ffd07826b6d2
Martin Nilsson <ibbles@hotmail.com>
2016-11-19 23:12:48 +0100

    Provide a list instead of a textbox in getRecipe.

--- routes/getRecipe.js
+++ routes/getRecipe.js
@@ -3,8 +3,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.render('getRecipe');
+var fs = require('fs');
+
+router.get('/', function(request, response) {
+    fs.readdir('Recipes/', function(error, files) {
+        if (error) { response.end(error); }
+        response.render('getRecipe', {files: files});
+    });
 });
 
 module.exports = router;
