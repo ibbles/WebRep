c6e70ce166fbc3f1c4cc59afcaf7e8f410886265
ibbles <ibbles@hotmail.com>
2021-04-13 20:07:57 +0200

    Add AddUser and DeleteUser

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,13 +1,14 @@
 // Userlist data array for filling in info box
 var userListData = [];
-console.log("Parsing global.js");
 
 // DOM Ready =============================================================
 $(document).ready(function() {
   // Populate the user table on initial page load
   populateTable();
 
-  $('#userList table tbody').on('click', 'td a.linshowuser', showUserInfo);
+  $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+  $('#btnAddUser').on('click', addUser);
+  $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 
 // Functions =============================================================
@@ -47,3 +48,72 @@ function showUserInfo(event) {
   $('#userInfoGender').text(thisUserObject.gender);
   $('#userInfoLocation').text(thisUserObject.location);
 };
+
+function addUser(event) {
+  event.preventDefault();
+
+  var haveChecked = false;
+  var errorCount = 0;
+  $('#addUser input').each(function(index, val) {
+    haveChecked = true;
+    if ($(this).val() === '') {
+      errorCount++;
+    }
+  });
+
+  if (!haveChecked) {
+    alert('Error: Did not do any verification.');
+  }
+
+  if (errorCount === 0) {
+    var newUser = {
+      'username': $('#addUser fieldset input#inputUserName').val(),
+      'email': $('#addUser fieldset input#inputUserEmail').val(),
+      'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+      'age': $('#addUser fieldset input#inputUserAge').val(),
+      'location': $('#addUser fieldset input#inputUserLocation').val(),
+      'gender': $('#addUser fieldset input#inputUserGender').val()
+    };
+
+    $.ajax({
+      type: 'POST',
+      data: newUser,
+      url: '/users/adduser',
+      dataType: 'JSON'
+    }).done(function(response) {
+      if (response.msg === '') {
+        $('#addUser fieldset input').val('');
+        populateTable();
+      }
+      else {
+        alert('Error: ' + response.msg);
+      }
+    });
+  }
+  else {
+    alert('Please fill in all fields.');
+    return false;
+  }
+}
+
+
+function deleteUser(event) {
+  event.preventDefault();
+
+  var confirmation = confirm('Are your sure?');
+  if (confirmation === true) {
+    $.ajax({
+      type: 'DELETE',
+      url: '/users/deleteuser/' + $(this).attr('rel')
+    }).done(function(response) {
+      if (response.msg !== '') {
+        alert("Error: " + response.msg);
+      }
+      populateTable();
+    });
+  }
+  else
+  {
+    return false;
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,21 @@ router.get('/userlist', function(req, res, next) {
   });
 });
 
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  collection.insert(req.body, function(err, result) {
+    res.send((err === null) ? {msg: ''} : {msg: err});
+  });
+});
+
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('userlist');
+  var userToDelete = req.params.id;
+  collection.remove({'_id': userToDelete}, function(err) {
+    res.send((err === null) ? {msg: ''} : {msg: "Error: " + err});
+  });
+});
+
 module.exports = router;
